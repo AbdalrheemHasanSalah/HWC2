9b4a3005f98b34517760716b4716c937a5a31e30
56165abd123 <abdsalahhasan2003@gmail.com>
2024-11-02 21:11:38 +0200

    Update _3.cpp

--- lapc2/_3.cpp
+++ lapc2/_3.cpp
@@ -1,77 +1,84 @@
-
+// hw4c2lap.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #include <iostream>
 #include<math.h>
-#include<string.h>
-
-
+#include <fstream>
 using namespace std;
-
-struct Account {
-
-	double balance;
-	Account() {
-		balance = 10;
-	}
-void withdraw(double value) {
-	cout << "press a if wont add and w if wont to withdraw  ";
-	char x;
-	cin >> x;
-	if (x == 'a') {
-		balance += value;
-	}
-	if (x == 'w') {
-		if (balance > value)
-			balance -= value;
-		else if (balance < value)
-			balance -= 5;
-	}
-}
-
-int inquire() {
-	return balance;
-}
-};
-
 int main()
 {
-	Account a;
-	int x, value;
-	cout << "press 1 if wont (add or withdraw) and 2 for inquire  ";
-	cin >> x;
-	while (true)
+	char* name[4];
+	char* name_city[4];
+	int grade[4];
+	ofstream a;
+	for (int i = 0; i < 4; i++)
 	{
-	switch (x)
-	{
-	case 1:
-	{
-		cout << "input value to do (add or withdraw) ";
-		cin >> value;
-		a.withdraw(value);
-		cout << "balance is:" << a.inquire() << endl;;
-		break;
+		name[i] = new char[100];
+		name_city[i] = new char[100];
 	}
-	case 2: {
-		cout <<"balance is:" << a.inquire() << endl;;
-		break;
+	strcpy(name[0], "Maha");
+	strcpy(name[1], "Reema");
+	strcpy(name[2], "Rami");
+	strcpy(name[3], "Sami");
+
+	grade[0] = 99;
+	grade[1] = 77;
+	grade[2] = 88;
+	grade[3] = 97;
+	strcpy(name_city[0], "Nablus");
+	strcpy(name_city[1], "Ramallh");
+	strcpy(name_city[2], "Jenin");
+	strcpy(name_city[3], "Bethlehem");
 
 
-	}
-	case 0:
-	{	exit(0);
-	break;
-	}
-	default:
-	{
-		cout << "press 1 if wont (add or withdraw) and 2 for inquire  ";
-		cin >> x;
-		break;
-	}
-	}
+	a.open("test.txt");
+	if (a.is_open()) {
+		cout << "file  is open to write ";
 
-	cout << "press 1 if wont (add or withdraw) and 2 for inquire  ";
-	cin >> x;
+
+
+		for (int i = 0; i < 4; i++) {
+			a << name[i] << " " << grade[i] << " " << name_city[i] << endl;
+		}
+	}
+	else
+		cout << "file is not open to write";
+
+
+	
+	char* p;
+	int count = 0;
+	p = new char[100];
+	char* p2;
+	p2 = new char[100];
+	int grad;
+	cin >> p;
+	cout << endl;
+	cin >> grad;
+	cout << endl;
+
+	ifstream b;
+	b.open("test.txt");
+	if (b.is_open()) {
+		cout << "file is open to read";
+		string c;
+		while (!b.eof()) {
+			b >> c;
+
+			while (b.getline(p2, 1000)) {
+				count++;
+				if (name[count] == p)grade[count] += grad;
+
+			}
+		}
+	}
+	else
+		cout << "file is not open to read";
+
+
+	for (int i = 0; i < 4; i++) {
+		a << name[i] << " " << grade[i] << " " << name_city[i] << endl;
+	}
+	a.close();
+	b.close();
 }
-}
-
-
